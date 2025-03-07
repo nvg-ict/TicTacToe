1c4924ff5b4e3bfdc8ca1d075d2b15d45c236334
nvg-ict <116808apd@gmail.com>
2025-03-07 16:10:14 +0100

    [RED] Create test to get an empty board

--- src/test/kotlin/BoardTest.kt
+++ src/test/kotlin/BoardTest.kt
@@ -1,11 +1,33 @@
 import nl.ns.dojo.Board
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertIs
 
 class BoardTest {
     @Test
-    fun `Given an instance of Board Then instance is created`() {
+    fun `Given an instance of Board, Then instance is created`() {
+        // Given
         val board = Board()
+
+        // Then
         assertIs<Board>(board)
+    }
+
+    @Test
+    fun `Given an empty Board, Then an empty 3 by 3 matrix is returned`() {
+        // Given
+        val board = Board()
+
+        val expectedBoard = arrayOf(
+            arrayOf(" ", " ", " "),
+            arrayOf(" ", " ", " "),
+            arrayOf(" ", " ", " ")
+        )
+
+        // Then
+        assertEquals(
+            expected = expectedBoard,
+            actual = board.getBoard()
+        )
     }
 }
