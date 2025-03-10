ca64e9b1c9325d314fb2826503bc406cebb9cef5
nvg-ict <116808apd@gmail.com>
2025-03-10 10:24:44 +0100

    [RED] Added test case when all fields are full

--- src/test/kotlin/BotTest.kt
+++ src/test/kotlin/BotTest.kt
@@ -1,6 +1,7 @@
 import nl.ns.dojo.Bot
 import kotlin.test.Test
 import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
 class BotTest {
@@ -22,5 +23,22 @@ class BotTest {
         val (row, col) = move
         assertTrue(row in 0..2)
         assertTrue(col in 0..2)
+    }
+
+    @Test
+    fun `Given an full board, when the bot generates a move, then null is returned`() {
+        // Given
+        val bot = Bot()
+        val board = arrayOf(
+            arrayOf("X", "X", "X"),
+            arrayOf("X", "X", "X"),
+            arrayOf("X", "X", "X"),
+        )
+
+        // When
+        val move = bot.getRandomMove(board)
+
+        // Then
+        assertNull(move)
     }
 }
