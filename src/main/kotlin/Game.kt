2e609e86855cffa5f0f18d15b04adb5c7924ba59
nvg-ict <116808apd@gmail.com>
2025-03-10 09:39:53 +0100

    [GREEN] Implemented isWon function

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,8 +1,22 @@
 package nl.ns.dojo
 
-class Game(board: Board) {
+class Game(val board: Board) {
     fun isWon(): Boolean {
-        return true
-    }
+        val board = board.getBoard()
+        // Check rows
+        for (row in board) {
+            if (row[0] != " " && row[0] == row[1] && row[1] == row[2]) {
+                return true
+            }
+        }
 
+        // Check columns
+        for (col in 0 until 3) {
+            if (board[0][col] != " " && board[0][col] == board[1][col] && board[1][col] == board[2][col]) {
+                return true
+            }
+        }
+
+        return false
+    }
 }
