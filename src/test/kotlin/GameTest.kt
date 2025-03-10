2687c223c765b6195be5f87a207474d389922aca
nvg-ict <116808apd@gmail.com>
2025-03-10 09:21:45 +0100

    [RED] Add test for Game

---
+++ src/test/kotlin/GameTest.kt
@@ -0,0 +1,13 @@
+import kotlin.test.Test
+import kotlin.test.assertIs
+
+class GameTest {
+    @Test
+    fun `Given an instance of Game, Then instance is created`() {
+        // Given
+        val game = Game()
+
+        // Then
+        assertIs<Game>(game)
+    }
+}
