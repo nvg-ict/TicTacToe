510278faf34c2ce2bac49e491aad8db0ba90583c
nvg-ict <116808apd@gmail.com>
2025-03-10 10:26:44 +0100

    [REFACTOR] Added test case for partially filled board

--- src/test/kotlin/BotTest.kt
+++ src/test/kotlin/BotTest.kt
@@ -1,14 +1,12 @@
 import nl.ns.dojo.Bot
-import kotlin.test.Test
-import kotlin.test.assertNotNull
-import kotlin.test.assertNull
-import kotlin.test.assertTrue
+import kotlin.test.*
 
 class BotTest {
+    private val bot = Bot()
+
     @Test
     fun `Given an empty board, when the bot generates a move, then a position is given`() {
         // Given
-        val bot = Bot()
         val board = arrayOf(
             arrayOf(" ", " ", " "),
             arrayOf(" ", " ", " "),
@@ -28,7 +26,6 @@ class BotTest {
     @Test
     fun `Given an full board, when the bot generates a move, then null is returned`() {
         // Given
-        val bot = Bot()
         val board = arrayOf(
             arrayOf("X", "X", "X"),
             arrayOf("X", "X", "X"),
@@ -40,5 +37,23 @@ class BotTest {
 
         // Then
         assertNull(move)
+    }
+
+    @Test
+    fun `Given a partially filled board, When bot moves, Then move is in an empty cell`() {
+        // Given
+        val board = arrayOf(
+            arrayOf("X", " ", "O"),
+            arrayOf(" ", "X", " "),
+            arrayOf("O", " ", "X")
+        )
+
+        // When
+        val move = bot.getRandomMove(board)
+
+        // Then
+        assertNotNull(move)
+        val (row, col) = move
+        assertEquals(" ", board[row][col])
     }
 }
